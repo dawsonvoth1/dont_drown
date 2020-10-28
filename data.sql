7ccab34fc6992df04d4ef452d552dc15be4c9699
dawsonvoth1 <70551506+dawsonvoth1@users.noreply.github.com>
2020-10-28 17:27:22 -0600

    fixes

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-CREATE TABLE players(
+CREATE TABLE player(
   player_id SERIAL PRIMARY KEY,
   description VARCHAR(50)
 );
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,51 +1,51 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 import './App.css';
-// import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import React, { Fragment } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+//import React, { Fragment } from "react";
 
-// import { HomeScreen } from './pages/HomeScreen';
-// import { SettingsScreen } from './pages/SettingsScreen';
+import { HomeScreen } from './pages/HomeScreen';
+import { SettingsScreen } from './pages/SettingsScreen';
 
-// export const router = React.createRef()
-
-// function App() {
-
-//   // declare initial screen to view and app navigator
-//   const [screenNav, setScreenNav] = useState('home');
-
-//   // return the screen to display based on value held by screenNav
-//   const getView = () => {
-//     if (screenNav === 'home'){
-//       return <HomeScreen setScreenNav={setScreenNav}/>
-//     }else if (screenNav === 'settings'){
-//       return <SettingsScreen setScreenNav={setScreenNav}/>
-//     }
-//   }
-
-//   // declare browser router to navigate to screen based on screen choice
-// 	return (
-// 		<BrowserRouter
-// 			ref={(x) => {
-// 				router.current = x
-// 			}}
-// 		>
-//       {getView()}
-// 		</BrowserRouter>
-// 	);
-// }
-
-import AddPlayer from "./pages/HomeScreen/AddPlayer";
+export const router = React.createRef()
 
 function App() {
-  return (
-    <Fragment>
-      <body>
-        <div className="container">
-          <AddPlayer />
-        </div>
-      </body>
-    </Fragment>
-  );
+
+  // declare initial screen to view and app navigator
+  const [screenNav, setScreenNav] = useState('home');
+
+  // return the screen to display based on value held by screenNav
+  const getView = () => {
+    if (screenNav === 'home'){
+      return <HomeScreen setScreenNav={setScreenNav}/>
+    }else if (screenNav === 'settings'){
+      return <SettingsScreen setScreenNav={setScreenNav}/>
+    }
+  }
+
+  // declare browser router to navigate to screen based on screen choice
+	return (
+		<BrowserRouter
+			ref={(x) => {
+				router.current = x
+			}}
+		>
+      {getView()}
+		</BrowserRouter>
+	);
 }
+
+// import AddPlayer from "./pages/HomeScreen/AddPlayer";
+
+// function App() {
+//   return (
+//     <Fragment>
+//       <body>
+//         <div className="container">
+//           <AddPlayer />
+//         </div>
+//       </body>
+//     </Fragment>
+//   );
+// }
 
 export default App;
